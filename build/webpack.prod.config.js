00cd80c2496e19f4dd7ce1329c914ab1e91c708f
zhaoxiang <756958008@qq.com>
2018-02-27 22:23:27 +0800

    delete 删除默认的title设置

--- build/webpack.prod.config.js
+++ build/webpack.prod.config.js
@@ -63,7 +63,7 @@ module.exports = merge(webpackBaseConfig, {
             }
         ]),
         new HtmlWebpackPlugin({
-            title: 'iView admin v' + package.version,
+            title: 'ApiAdmin',
             favicon: './td_icon.ico',
             filename: '../index.html',
             template: '!!ejs-loader!./src/template/index.ejs',
