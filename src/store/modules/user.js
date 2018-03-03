6c6fa7ce70513e8e8c7ea3d77a59711161be0415
zhaoxiang <756958008@qq.com>
2018-03-04 00:38:35 +0800

    modified 优化退出

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,8 +1,7 @@
 const user = {
     mutations: {
         logout () {
-            sessionStorage.removeItem('access');
-            sessionStorage.removeItem('apiAuth');
+            sessionStorage.clear();
             // 恢复默认样式
             let themeLink = document.querySelector('link[name="theme"]');
             themeLink.setAttribute('href', '');
