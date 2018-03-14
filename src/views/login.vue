062f382e78d1427182548c17c4759e70d65857f0
zhaoxiang <zhaoxiang051405@gmail.com>
2018-03-14 18:02:19 +0800

    added 添加微信，qq登录资源

--- src/views/login.vue
+++ src/views/login.vue
@@ -8,20 +8,20 @@
             <Card :bordered="false">
                 <p slot="title">
                     <Icon type="log-in"></Icon>
-                    欢迎登录
+                    欢迎登录ApiAdmin后台管理系统
                 </p>
                 <div class="form-con">
                     <Form ref="loginForm" :model="form" :rules="rules">
                         <FormItem prop="userName">
                             <Input v-model="form.username" placeholder="请输入用户名">
-                                <span slot="prepend">
+                            <span slot="prepend">
                                     <Icon :size="16" type="person"></Icon>
                                 </span>
                             </Input>
                         </FormItem>
                         <FormItem prop="password">
                             <Input type="password" v-model="form.password" placeholder="请输入密码">
-                                <span slot="prepend">
+                            <span slot="prepend">
                                     <Icon :size="14" type="locked"></Icon>
                                 </span>
                             </Input>
@@ -30,7 +30,11 @@
                             <Button @click="handleSubmit" type="primary" long>登录</Button>
                         </FormItem>
                     </Form>
-                    <p class="login-tip">欢迎使用ApiAdmin3.0后台管理系统</p>
+                </div>
+                <div style="padding-top:10px;font-size: 11px;border-top: 1px solid #e9eaec;">
+                    <span style="vertical-align:middle;">其他登录：</span>
+                    <img style="vertical-align:middle;" src="../../src/images/qq_login.png" alt="">
+                    <img style="vertical-align:middle;" src="../../src/images/wx_login.png" alt="">
                 </div>
             </Card>
         </div>
@@ -38,48 +42,49 @@
 </template>
 
 <script>
-import axios from 'axios';
-export default {
-    data () {
-        return {
-            form: {
-                username: '',
-                password: ''
-            },
-            rules: {
-                username: [
-                    { required: true, message: '账号不能为空', trigger: 'blur' }
-                ],
-                password: [
-                    { required: true, message: '密码不能为空', trigger: 'blur' }
-                ]
-            }
-        };
-    },
-    methods: {
-        handleSubmit () {
-            this.$refs.loginForm.validate((valid) => {
-                if (valid) {
-                    let vm = this;
-                    axios.post('Login/index', {
-                        username: this.form.username,
-                        password: this.form.password
-                    }).then(function (response) {
-                        if (response.data.code === 1) {
-                            vm.$store.commit('login', response.data.data);
-                            vm.$Message.success(response.data.msg);
-                            vm.$router.push({
-                                name: 'home_index'
-                            });
-                        } else {
-                            vm.$Message.error(response.data.msg);
-                        }
-                    });
+    import axios from 'axios';
+
+    export default {
+        data () {
+            return {
+                form: {
+                    username: '',
+                    password: ''
+                },
+                rules: {
+                    username: [
+                        {required: true, message: '账号不能为空', trigger: 'blur'}
+                    ],
+                    password: [
+                        {required: true, message: '密码不能为空', trigger: 'blur'}
+                    ]
                 }
-            });
+            };
+        },
+        methods: {
+            handleSubmit () {
+                this.$refs.loginForm.validate((valid) => {
+                    if (valid) {
+                        let vm = this;
+                        axios.post('Login/index', {
+                            username: this.form.username,
+                            password: this.form.password
+                        }).then(function (response) {
+                            if (response.data.code === 1) {
+                                vm.$store.commit('login', response.data.data);
+                                vm.$Message.success(response.data.msg);
+                                vm.$router.push({
+                                    name: 'home_index'
+                                });
+                            } else {
+                                vm.$Message.error(response.data.msg);
+                            }
+                        });
+                    }
+                });
+            }
         }
-    }
-};
+    };
 </script>
 
 <style>
