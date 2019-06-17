daf6dc0680dbf17ddc0d62d1f71afeeb83d8f324
zhoubozhoubo <759226488@qq.com>
2019-06-17 10:59:12 +0800

    zb190617

--- src/views/task/task/task_list.vue
+++ src/views/task/task/task_list.vue
@@ -767,8 +767,8 @@
                                     size: 'large'
                                 },
                                 props: {
-                                    'true-value': 1,
-                                    'false-value': 0,
+                                    'true-value': '1',
+                                    'false-value': '0',
                                     value: currentRowData.status
                                 },
                                 on: {
