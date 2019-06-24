ff74f995461127eb412295ac8bb536172da97be2
zhoubozhoubo <759226488@qq.com>
2019-06-24 15:52:52 +0800

    zb

--- build/config.js
+++ build/config.js
@@ -1,7 +1,7 @@
 let config = {
     env: 'development',
     // baseUrl: 'http://127.0.0.1:8001/admin/'
-    // baseUrl: 'http://jianzhi.hmdog.com/admin/'
-    baseUrl: 'http://zaijia.huiyuancaifu.cn/admin/'
+    baseUrl: 'http://jianzhi.hmdog.com/admin/'
+    // baseUrl: 'http://zaijia.huiyuancaifu.cn/admin/'
 };
 export default config;
